90102008fc8c64a1af268a0603a38f13700632c0
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 20:26:41 +0500

    Add files via upload

---
+++ 21 Object.ts
@@ -0,0 +1,30 @@
+interface person {
+    age : number
+    name : string,
+    Student : Boolean
+}
+let person ={
+    age : 16,
+    name : 'tanjoo',
+    student : true,
+}
+
+console.log(person)
+
+// another example
+interface car {
+    Maker : string,
+    NAme : string,
+    colour : string,
+    Automatic : boolean
+}
+
+
+let car ={
+    Maker : 'Toyota',
+    Name : 'Supra mk4',
+    Color : 'Black',
+    Automatic : false
+}
+
+console.log(car)
