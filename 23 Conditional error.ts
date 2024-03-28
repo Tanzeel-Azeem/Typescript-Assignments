87e7c9ab6c19883b6e34409a4f0f79365f2da8b3
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 20:27:45 +0500

    Add files via upload

---
+++ 23 Conditional error.ts
@@ -0,0 +1,41 @@
+let car : string = "subaru";
+// test no.1
+
+console.log("Is car ==='subaru'?I predict true")
+console.log(car ==='subaru');
+
+// test no.2
+console.log("Is car =='subaru'?I predict true")
+console.log(car =='subaru');
+
+// test no.3
+console.log("Is car !=='Honda'?I predict true")
+console.log(car !=='Honda');
+
+// test no.4 
+console.log("Is car !=='Ford'?I predict true")
+console.log(car !=='Ford');
+
+// test Node.5
+console.log("Is car.uppercase =='SUBARU'?I predict true")
+console.log(car.toUpperCase() =='SUBARU');
+
+// test Node.6
+console.log("Is car =='SUBARU'?I predict false")
+console.log(car =='SUBARU');
+
+// TEST Node.7
+console.log("Is car ==='SUBARU'?I predict false")
+console.log(car ==='SUBARU');
+
+// test no.8
+console.log("Is car ==='bike'?I predict false")
+console.log(car ==='bike');
+
+// test no.9
+console.log("Is car ==='Cycle'?I predict false")
+console.log(car ==='Cycle');
+ 
+// test no.10
+console.log("Is car ==='Plane'?I predict false")
+console.log(car ==='Plane');
