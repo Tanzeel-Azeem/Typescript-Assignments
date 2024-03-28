d0a6b0b11c055b268544dbe27cf40ba74ea24d00
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 20:25:08 +0500

    Add files via upload

---
+++ 18  seeing the world.ts
@@ -0,0 +1,18 @@
+let places:string []= ['Iran','Turkey','France','Agra','Jordan']
+console.log(places)
+
+console.log([...places].sort());
+
+console.log(places)
+
+console.log([...places].sort().reverse());
+
+console.log(places)
+
+console.log(places.reverse())
+
+console.log(places.reverse())
+
+console.log(places.sort())
+
+console.log(places.sort().reverse())
