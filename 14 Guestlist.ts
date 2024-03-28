2f0a6463b63d816a596944c25b9c24713356982e
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 20:22:24 +0500

    Add files via upload

---
+++ 14 Guestlist.ts
@@ -0,0 +1,5 @@
+let personName :string[]= ['ali','ibad','aqil','faisal']
+
+for(let i=0; i<personName.length; i++){
+    console.log('Dear Mr. ' + personName[i] + '\n\nIt is our pleasure to invite you in our party.\n\nThank you!\n\n')
+}
