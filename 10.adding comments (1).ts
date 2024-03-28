28a31784e72c9d7ba5e4a3d521c4de9d8a5e37b5
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 19:59:56 +0500

    Add files via upload

---
+++ 10.adding comments (1).ts
@@ -0,0 +1,8 @@
+// Program wriiten by : tanzzel
+//date : 8 - march - 2024
+//this program will write : Never give up
+
+
+
+
+console.log('Never give up')
---
+++ 10.adding comments (2).ts
@@ -0,0 +1,10 @@
+// program written by : TANZEEL
+//Date : 08-3-2024
+// this program will write that 69*69 is 4761
+
+
+
+
+
+
+console.log(69*69)
