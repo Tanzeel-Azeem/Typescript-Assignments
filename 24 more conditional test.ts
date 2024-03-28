3eaed51f0e548d2e703d05b36a00d13f60d1a886
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-28 20:28:13 +0500

    Add files via upload

---
+++ 24 more conditional test.ts
@@ -0,0 +1,34 @@
+// equility test
+console.log('Equility test with String: ', "Apple"==="Apple");
+// Inequility test 
+console.log('Inequility test with String: ', ("Apple" as string)!=="Orange");
+
+
+// lowercase function
+console.log('Lowercase function test: ', "HELLO".toLowerCase()==="hello");
+
+// equility test with number
+console.log('Equility test with String: ', "Apple"==="Apple");
+// inequility test with number
+console.log('inequility test with String: ', (420 as number )!== 69);
+
+
+// greater than test
+console.log('Greater than test: ', 420>69);
+// less than test
+console.log('less than test: ', 69<420);
+
+
+
+// greater than or equal to teest
+console.log('greater than or equal test: ', 69 >= 69);
+// less than or equal to teest
+console.log('less than or equal test: ', 69  <=420 );
+
+
+
+// test using And operator
+console.log('Test using AND operator: ', 5===5 && 10>5)
+
+// test using OR operator
+console.log('test using OR operator: ', 5===5 || false)
