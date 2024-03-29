1dbfc298f26c6f146acf3c181c14011ae2bfbaef
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:31:17 +0500

    Add files via upload

---
+++ 38_Cities.ts
@@ -0,0 +1,8 @@
+function describe_city( city :string , country :string = 'Spain'){
+    console.log(`${city} is in ${country}`)
+}
+
+
+describe_city('Manchester' , 'England')
+describe_city('Bayeran' , 'Germany')
+describe_city('Madrid')
