66fb59c63f73c7df825c59243e3bece7f1a76396
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:22:58 +0500

    Add files via upload

---
+++ 28_stages_of_life.ts
@@ -0,0 +1,22 @@
+let age :number = 17
+if( age <2){
+
+    console.log('person is baby')
+}
+else if ( age >= 2 && age <4){
+    console.log("persin is a toddler")
+}
+else if (age >=4 && age< 13){
+    console.log("person is Kid")
+}
+else if (age >=13&& age<20 ){
+    console.log("person is teenager")
+}
+
+else if (age >=20 && age< 65){
+    console.log("person is adult")
+}
+
+else {
+    console.log("Person is elder")
+}
