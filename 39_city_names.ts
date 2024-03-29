bcea7e0a6f4ea2d9b68fb106733820073523a3ef
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:31:48 +0500

    Add files via upload

---
+++ 39_city_names.ts
@@ -0,0 +1,10 @@
+function city_country(city :string, country:string){
+
+return`${city},${country}`
+
+}
+console.log(city_country('Karachi','Pakistan'))
+
+console.log(city_country('Madird','Spain'))
+
+console.log(city_country('Bayeran','Germany'))
