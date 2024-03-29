6f7430f16d6ec84df9024f8df30b3e93cc589e76
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:34:16 +0500

    Add files via upload

---
+++ 44_sandwish.ts
@@ -0,0 +1,12 @@
+function make_sandwich(item : string[]){
+    console.log('\n\nMaking your Sandwich with : ')
+
+ item.forEach(item => console.log("-" +item ));
+
+ console.log('Enjoy your Sandwich !!!\n\n')
+}
+
+ make_sandwich(['ham','Cheese','Lettuce'])
+ make_sandwich(['mayo','Beacon',])
+ make_sandwich(['buuter','turkey','tamato'])
+
