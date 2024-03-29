57e84ed4de0389b983d68e7a2e56deadf6c8eb04
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:24:25 +0500

    Add files via upload

---
+++ 29_favourite_fruit.ts
@@ -0,0 +1,21 @@
+let favourite_fruits : string[]= ['apple','mango','banana']
+
+if(favourite_fruits.includes('apple')){
+    console.log("I really like Apple!")
+}
+
+if(favourite_fruits.includes('mango')){
+    console.log("I really like MAngo!")
+}
+
+if(favourite_fruits.includes('banana')){
+    console.log("I really like banana!")
+}
+
+if(favourite_fruits.includes('kiwi')){
+    console.log("I really like kiwi!")
+}
+
+if(favourite_fruits.includes('oranges')){
+    console.log("I really like oranges!")
+}
