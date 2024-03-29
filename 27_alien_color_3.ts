30966a92fc1c4d8c3e8f710cb751006473c67322
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:22:32 +0500

    Add files via upload

---
+++ 27_alien_color_3.ts
@@ -0,0 +1,54 @@
+let alienColor:string = 'green'
+//  v1
+if (alienColor === 'green'){
+    console.log("Player just earned 5 points!")
+}else if(alienColor=== 'yellow'){
+    console.log("Player just earned 10 ponts")
+}else if (alienColor ===" red"){
+    console.log("Player just earned 15 points")
+}
+else{
+    console.log("Please select the right colour")
+}
+
+
+// v2
+alienColor ="yellow"
+
+if (alienColor === 'green'){
+    console.log("Player just earned 5 points!")
+}else if(alienColor=== 'yellow'){
+    console.log("Player just earned 10 ponts")
+}else if (alienColor ===" red"){
+    console.log("Player just earned 15 points")
+}
+else{
+    console.log("Please select the right colour")
+}
+
+// v3
+alienColor = "red"
+
+if (alienColor === 'green'){
+    console.log("Player just earned 5 points!")
+}else if(alienColor=== 'yellow'){
+    console.log("Player just earned 10 ponts")
+}else if (alienColor ==="red"){
+    console.log("Player just earned 15 points")
+}
+else{
+    console.log("Please select the right colour")
+}
+
+alienColor = " blue"
+
+if (alienColor === 'green'){
+    console.log("Player just earned 5 points!")
+}else if(alienColor=== 'yellow'){
+    console.log("Player just earned 10 ponts")
+}else if (alienColor ===" red"){
+    console.log("Player just earned 15 points")
+}
+else{
+    console.log("Please select the right colour")
+}
