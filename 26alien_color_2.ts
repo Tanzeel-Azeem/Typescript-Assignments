698914ec7a6fcea9d0b7235d6b1be5b70f036282
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:22:05 +0500

    Add files via upload

---
+++ 26alien_color_2.ts
@@ -0,0 +1,17 @@
+let alienColor:string = "green"
+if(alienColor === "green"){
+    console.log("player just earned 5 points")
+}else{
+    console.log("Player just earned 10 points")
+}
+
+
+
+
+
+alienColor = 'yellow'
+if (alienColor === "green"){
+    console.log("player just earned 5 points")
+}else{
+    console.log("Player just earned 10 points")
+}
