0ac2b6b580d86448d9ebaebf567a87c52d12a4b6
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:29:35 +0500

    Add files via upload

---
+++ 36_T-shirts.ts
@@ -0,0 +1,10 @@
+function make_shirt(size :string , text : string){
+
+    console.log(`Creating a ${size} with the message : ${text}`);
+    
+}
+make_shirt( 'large' , 'SUUUIIIIIIII')
+
+make_shirt( 'Medium' , 'SUUUIIIIIIII')
+
+make_shirt( 'Small' , 'SUUUIIIIIIII')
