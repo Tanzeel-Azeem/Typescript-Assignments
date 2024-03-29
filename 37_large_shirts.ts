ecf63337be6e026a0e9455c8b9ddf5922ffae370
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:30:39 +0500

    Add files via upload

---
+++ 37_large_shirts.ts
@@ -0,0 +1,9 @@
+function make_shirt(size : string = 'Large', text : string = 'I love typescript')  {
+    console.log(`Creating a ${size} Shirt with the message : ${text}`);
+}
+
+make_shirt()
+
+make_shirt('Medium')
+
+make_shirt('XXL' , 'SUIIII')
