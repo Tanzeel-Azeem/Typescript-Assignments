07a9c6e8c6d06f581836bfb1e23d5f0f5888a3d0
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:32:51 +0500

    Add files via upload

---
+++ 41_magician.ts
@@ -0,0 +1,11 @@
+let magician :string []= ['Dr.Strange', 'Dormamu', 'Luka Modric','KDB','Kong'];
+
+
+function show_magicians(magicians:string []){
+    magicians.forEach(element => {
+       console.log(element);
+        
+    });
+}
+
+show_magicians(magician)
