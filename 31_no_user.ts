eae1be458f244c3933672eafaa25345d54ded97d
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-03-29 12:25:52 +0500

    Add files via upload

---
+++ 31_no_user.ts
@@ -0,0 +1,8 @@
+let userNames: string[]= ['Admin', 'ali','khan','ayan','ibad'];
+if(userNames.length === 0){
+    console.log('We need to find some users!!!')
+}else {
+    userNames= [];
+
+    console.log('All the users names have been removed ' + userNames.length)
+}
