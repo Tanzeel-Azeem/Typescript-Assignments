fc143698363d57da8592bdb1596dca0c0230fdc2
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-05-24 14:20:38 +0500

    Add files via upload

---
+++ name_cases.ts
@@ -0,0 +1,19 @@
+let personName : string = 'TaNZEEl aZEem'
+
+let lowerCase = personName.toLowerCase()
+
+let upperCase = personName.toUpperCase()
+
+let titleCase = personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()
+
+
+
+ 
+
+
+console.log(personName);
+console.log(lowerCase);
+console.log(upperCase);
+console.log(titleCase);
+
+
