cf5bd31fb866bbf942a48528fc24cf9aee0f371c
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-05-25 21:20:21 +0500

    Delete 35_animals.ts

--- 35_animals.ts
+++
@@ -1,7 +0,0 @@
-let Animals:string[]= ['dog','cat','GOAT']
-for(let animal of Animals){
-
-console.log(`A ${animal} would make a great pet!`);
-}
-
-console.log('Any of them would make a great pet!')
