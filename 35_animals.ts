f006fb238cf9aac3cae9913387b525227ec635ed
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-05-27 18:20:49 +0500

    Add files via upload

---
+++ 35_animals.ts
@@ -0,0 +1,9 @@
+let Animals:string[]= ['dog','cat','Rabbit']
+
+
+for(let i=0 ;i<Animals.length; i++){
+
+console.log(`A ${Animals[i]} would make a great pet...because they are Friendly!`);
+}
+
+console.log('Any of them would make a great pet!')
