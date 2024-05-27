6969f36d3ead811ab6a0e1dd824d899e1bce0e83
Tanzeel-Azeem <164767590+Tanzeel-Azeem@users.noreply.github.com>
2024-05-27 18:19:42 +0500

    Add files via upload

---
+++ 34_pizzas.ts
@@ -0,0 +1,6 @@
+let pizzas:string[]= ['BBQ','Fajita','Malai boti'];
+for(let i=0;i<pizzas.length;i++){
+    console.log(`I like ${pizzas} pizza!`);
+}
+console.log('I really love pizza!')
+console.log('All kind of pizzas are delicious but the one which i have Stated are Amazing');
